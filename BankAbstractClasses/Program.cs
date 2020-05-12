f1b5094db313b12a2dd60c690b8cc3dc6da8f488
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-12 13:32:38 -0500

    first commit

---
+++ BankAbstractClasses/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAbstractClasses
+{
+    public interface IAccount
+    {
+        //void PayInFunds(decimal amount);
+        //bool WithdrawFunds(decimal amount);
+        decimal GetBalance();
+        string RudeLetterString();
+    }
+
+    public abstract class Account:IAccount
+    {
+        private decimal balance = 0;
+
+        public abstract string RudeLetterString();//abstract=will be described+implemented in child classes
+                                                  //different way in each child class
+        public decimal GetBalance()
+        { return balance; }
+    }
+
+    public class CustomerAccount:Account
+    {
+        public override string RudeLetterString ()
+        {
+            string str ="";
+
+            Console.WriteLine("you account is overdrawn!");
+            return str;  
+        }
+    }
+
+    public class BabyAccount : Account, IAccount
+    {
+        public override string RudeLetterString()
+        {
+            string str = "";
+
+            Console.WriteLine("you BabyAccount is overdrawn!");
+            return str;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int MAX_CUST = 100;
+
+            IAccount[] accounts = new IAccount[MAX_CUST];
+            accounts[0] = new CustomerAccount();
+            accounts[0].RudeLetterString();
+
+            accounts[1] = new BabyAccount();
+            accounts[1].RudeLetterString();
+
+            Console.ReadKey();
+        }
+    }
+}
